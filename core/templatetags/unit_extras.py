09b9f53b54e36c707eaa4f6b4605f2c8d7fe3833
djupp <manuelkaeppler@gmail.com>
2014-08-07 12:55:59 -0400

    Template tags file for Unit models added

---
+++ core/templatetags/unit_extras.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(name='unitconvert')
+def unitconversion(amount, *args, **kwargs):
+    curUnit = kwargs['current']
+    newUnit = kwargs['new']
+    return curUnit.convert_to(newUnit, amount)
