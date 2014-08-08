f48eb87ecf5f7414b8f2d8f44992275e5d7fa349
djupp <manuelkaeppler@gmail.com>
2014-08-08 14:17:40 -0400

    Added PurchaseItem model which allows for several products to be purchased in one Purchase object

--- shopping/models.py
+++ shopping/models.py
@@ -13,19 +13,23 @@ class Store(models.Model):
 
 
 class Product(Good):
-
-    store = models.ManyToManyField(Store, through="Purchase",
-                                   through_fields=('what', 'where'))
+    pass
 
 
 class Purchase(models.Model):
 
     where = models.ForeignKey(Store)
-    what = models.ForeignKey(Product)
     date = models.DateField()
-    amount = models.DecimalField(max_digits=4, decimal_places=1)
-    unit = models.ForeignKey(Unit)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    items = models.ManyToManyField(Product, through="PurchaseItem",
+                                  through_fields=('purchase', 'product'))
 
     def get_absolute_url(self):
         return reverse('PurchaseDetail', kwargs={'pk': self.pk})
+
+class PurchaseItem(models.Model):
+
+    purchase = models.ForeignKey(Purchase)
+    product = models.ForeignKey(Product)
+    amount = models.DecimalField(max_digits=4, decimal_places=1)
+    unit = models.ForeignKey(Unit)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
